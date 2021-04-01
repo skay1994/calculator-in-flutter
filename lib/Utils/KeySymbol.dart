e7dd3604810dceb0b4dbbd726db85a0899f92103
Jorge Carlos <skay_1994@yahoo.com.br>
2021-04-01 18:22:36 -0300

    Added factory KeySymbol.asEmpty

--- lib/Utils/KeySymbol.dart
+++ lib/Utils/KeySymbol.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'Colors.dart';
+
 enum KeyType { FUNCTION, OPERATOR, INTEGER }
 
 class KeySymbol {
-  const KeySymbol(this.value, this.type, this.color);
+  const KeySymbol(this.value, [this.type, this.color]);
+
+  factory KeySymbol.asEmpty() {
+    return KeySymbol('is_empty');
+  }
 
   final String value;
-  final Color color;
-  final KeyType type;
+  final Color? color;
+  final KeyType? type;
+
+  bool isEmptyKey() => value == 'is_empty';
 
   @override
   String toString() => value;
